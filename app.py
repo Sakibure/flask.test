72da800dd752ec8b63f23ef11778765cf7969f6f
Sakibure <victor526040181@gmail.com>
2021-08-16 15:56:59 +1200

    test

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Flask Test File: hello world'
+    @app.route('/')
+    def index():
+        return 'Index page'
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/user/<username>')
+    def show_user_profile(username):
+        # Show the user profile for that user
+        return 'User %s' % escape(username)
+
+    @app.route('/post/<int:post_id>')
+    def show_post(post_id):
+        # Show the post with the given id, the id is an integer
+        return 'Post %d' % post_id
+
+    @app.route('/path/<path:subpath>')
+    def show_subpath(subpath):
+        # Show the subpath after /path/
+        return 'Subpath %s' % escape(subpath)
+
+    @app.route('/news', methods=['GET'])
+    def show_news():
+        return "Today’s news is ..."
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(host='localhost', port=5000)
